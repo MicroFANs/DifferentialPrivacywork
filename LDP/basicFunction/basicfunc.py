056d7bf15eb4ba5e0b3d7698c05034a241123dea
范泽轩 <35322505+MicroFANs@users.noreply.github.com>
2019-12-12 21:56:30 +0800

    增加LDPMiner的两个指标RE和DCG

--- LDP/basicFunction/basicfunc.py
+++ LDP/basicFunction/basicfunc.py
@@ -53,4 +53,76 @@ def one_hot_1D(np_data):
     return onehot_data
 
 
+def Relative_Error(actual,estimate):
+    """
+    LDPMiner论文中的相对误差指标，用来评价top-k中估计频率的误差
+    :param actual: heavyhitter的真实频率，numpy数组
+    :param estimate: 频率估计，numpy数组
+    :return: 相对误差RE
+    """
+    error=np.abs(estimate-actual)/actual
+    RE=np.median(error)
+    return RE
+
+
+def rel(d,k,rank_true,rank_estimate):
+    """
+    LDPMiner论文中排名误差指标DCG中的参数rel
+    :param d: 数据维度
+    :param k: topk的k
+    :param rank_true:真实排序 长度>=k
+    :param rank_estimate: 估计得到的排序 长度>=k
+    :return: rel数组，真实排序中每个项的rel值
+    """
+    topk_act=np.zeros(k) # 真实topk中每个元素v的rank值
+    topk_est=np.zeros(k) # 估计得到的topk中对应的元素v的rank值
+    for i in range(k):
+        v=rank_true[i] # 遍历真实排序rank_true中的前k个项
+
+        index_true=np.where(rank_true==v)
+        topk_act[i]=index_true[0][0]+1   # 生成真实topk项的rank值
+
+        index_est=np.where(rank_estimate==v) # 生成估计topk项的rank值
+        topk_est[i]=index_est[0][0]+1
+
+    print(topk_act,topk_est)
+    rel=np.log2(np.abs(d-np.abs(topk_act-topk_est)))
+    return rel
+
+def DCG(d,k,rank_true,rank_estimate):
+    rel_v=rel(d,k,rank_true,rank_estimate)
+    print(rel_v)
+    sum=rel_v[0]
+    for i in range(1,k):
+        temp=rel_v[i]/np.log2(i+1)
+        sum=sum+temp
+    dcg=sum
+
+    return dcg
+
+def IDCG(d,k):
+    rel_v=np.log2(d)*np.ones(k)
+    print(rel_v)
+    sum=rel_v[0]
+    for i in range(1,k):
+        temp=np.log2(d)/np.log2(i+1)
+        sum=sum+temp
+    idcg=sum
+
+    return idcg
+
+
+
+true=np.array([1,2,3,4,5,6,7,8,9,10])
+
+est=np.array([4,2,1,3,5,7,10,8,6,9])
+
+
+
+
+dcg=DCG(64,6,true,est)
+idcg=IDCG(64,6)
+
+print('DCG',dcg)
+print('IDCG',idcg)
 
