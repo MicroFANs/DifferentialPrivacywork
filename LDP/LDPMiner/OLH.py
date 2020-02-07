605b7ea738c8021383ea78e3f57f12306104b2cf
范泽轩 <35322505+MicroFANs@users.noreply.github.com>
2020-02-07 22:43:21 +0800

    添加了原作者实现的函数
    添加作者实现的olh_hash、olh_support、olh_grr函数，相同数据集下，运行时间是我自己写的三分之一，效率大大增高

--- LDP/LDPMiner/OLH.py
+++ LDP/LDPMiner/OLH.py
@@ -5,6 +5,7 @@
 """
 
 import matplotlib
+
 matplotlib.use('TkAgg')
 import LDP.basicDP.RPbasic as rpb
 import LDP.basicDP.SHbasic as shb
@@ -14,70 +15,80 @@ import numpy as np
 import pandas as pd
 import random
 import matplotlib.pyplot as plt
+import time
 
 # 关闭科学计数法显示
 np.set_printoptions(suppress=True)
 
-
 # Locally Differentially Private Protocols for Frequency Estimation
 # 论文中的OLH算法
 
-epsilon=2
-g=int(np.exp(epsilon)+1)
-#print(g)
-
-path='../LDPMiner/dataset/kosarak/kosarak_10k_singlevalue.csv'
-user_data=bf.readcsv(path)
-#print(user_data)
-n=len(user_data)
-#print(n)
 
 
+path = '../LDPMiner/dataset/kosarak/kosarak_10k_singlevalue.csv'
+user_data = bf.readcsv(path)
+# print(user_data)
+n = len(user_data)
+# print(n)
+
+
+epsilon = 2
+g = int(np.exp(epsilon) + 1)
+# print(g)
+
+# 程序运行起始时间
+starttime=time.clock()
 
 
 """
 原始数据，用来计算误差
 """
-list_true=bf.csvtolist(path) # list_true是真实的数据list
-#print(list_true)
+list_true = bf.csvtolist(path)  # list_true是真实的数据list
+# print(list_true)
 
 label = np.unique(user_data)
 x_list = label.tolist()
 
-
-
-
-
-
 """
 encoding
 """
-encode=np.zeros((n,g))
-x=[] # 这里面存的是
-#print(encode)
+encode = np.zeros((n, g))
+x = []  # 这里面存的是
+# print(encode)
 for i in range(n):
-    value=user_data[i][0]
-    index=bf.hash(value,g,i)
-    #encode[i][index]=1
+    value = user_data[i][0]
+
+    # 自己写的hash函数
+    #index = bf.hash(value, g, i)
+
+    # 作者源码实现方式olh_hash,效果很差，不知道为什么。原因找到了，没有修改与之匹配的support函数
+    """替换hash函数时一定别忘记更换下面匹配的olh_support函数，一共要改3处，hash，support，grr"""
+    index = lhb.olh_hash(value, g, i)
+
+    # encode[i][index]=1
     x.append(index)
 
 # print(encode)
 # print(x)
-#bf.savecsv(encode,'../LDPMiner/dataset/kosarak/encode.csv')
-
+# bf.savecsv(encode,'../LDPMiner/dataset/kosarak/encode.csv')
 
 
 """
 perturbing
 """
-p=rpb.gen_probability(epsilon,n=g)
-q=p/np.e**epsilon
-#print(p,q)
+p = rpb.gen_probability(epsilon, n=g)
+q = p / np.e ** epsilon
+# print(p,q)
 
-y=[]
+y = []
 for i in range(n):
-    y.append(lhb.grr(p,x[i],g))
-#print(y)
+
+    # 自己实现的grr
+    #y.append(lhb.grr(p, x[i], g))
+    # 成作者源码实现方式olh_grr，但是作者实现的grr结果都一样，不会变，不知道为什么。答案是我自己的hash函数结果不会变，是因为用了系统时间种子
+     y.append(lhb.olh_grr(p, x[i], g))
+
+# print(y)
 
 """
 Aggregation
@@ -91,22 +102,32 @@ Aggregation
 # print(estimate/n)
 
 
-count_true=[] #真实计数结果
-count_estimate=[] # 估计计数结果
+count_true = []  # 真实计数结果
+count_estimate = []  # 估计计数结果
 for i in range(len(x_list)):
     count_true.append(list_true.count(x_list[i]))
 
-    c=lhb.support(x_list[i],y,g,n)
-    estimate=lhb.aggregation(c,n,g,p)
+    # 自己写的support函数匹配自己的hash函数
+    #c = lhb.support(x_list[i], y, g, n)
+    # 与作者源码的olh_hash匹配的support函数
+    """替换hash函数时一定别忘记更换下面匹配的olh_support函数"""
+    c = lhb.olh_support(x_list[i], y, g, n)
+
+
+    estimate = lhb.aggregation(c, n, g, p)
     count_estimate.append(estimate)
 
-print(count_true) #真实的计数结果
-print(count_estimate) # 估计的计数结果
+# 程序运行结束时间
+endtime=time.clock()
+
+
+print(count_true)  # 真实的计数结果
+print(count_estimate)  # 估计的计数结果
+print("运行时间：%s s"%(endtime-starttime))
 
 """
 保存结果
 """
-result={"OLH":count_estimate,"true":count_true,"value":x_list}
-df_res=pd.DataFrame(result)
-df_res.to_csv("../LDPMiner/dataset/kosarak/result/10k_sv_OLH.csv",index=False)
-
+result = {"OLH": count_estimate, "true": count_true, "value": x_list}
+df_res = pd.DataFrame(result)
+df_res.to_csv("../LDPMiner/dataset/kosarak/result/10k_sv_OLH.csv", index=False)
--- LDP/basicDP/OLHbasic.py
+++ LDP/basicDP/OLHbasic.py
@@ -8,26 +8,84 @@ import numpy as np
 import random
 import LDP.basicFunction.basicfunc as bf
 
+# 这里需要导入xxhash这个包
+import xxhash
 
-def grr(p,bit,g):
-    v= [i for i in range(g)]
-    rnd=np.random.random()
-    if rnd<=p:
-        perturbed_bit=bit
+
+def olh_hash(value, size, seed):
+    """
+    此olh_hash函数是作者源码里的实现方式
+    是使用了xxhash这个包来实现的
+    要区别于basicfunc包中我自己实现的hash函数，其实我感觉也差不多
+    :param value:输入的值
+    :param size: 哈希域的大小,就是g
+    :param seed: 种子,原文作者将用户的编号作为种子，即第一个用户为0，下一个为1
+    :return: 哈希得到的值，在（0，g）之间
+    """
+
+    h=(xxhash.xxh32(str(value), seed=seed).intdigest() % size)
+    return h
+
+
+def grr(p, bit, g):
+    v = [i for i in range(g)]
+    rnd = np.random.random()
+    if rnd <= p:
+        perturbed_bit = bit
     else:
-        del(v[bit])
-        perturbed_bit=random.choice(v)
+        del (v[bit])
+        perturbed_bit = random.choice(v)
     return perturbed_bit
 
 
-def support(v,report,g,n):
-    c=0
+def olh_grr(p, bit, g):
+    """
+    olh_grr是作者源码里实现扰动的方式
+    :param p: 保持不变的概率，即exp(epsilon) / (exp(epsilon) + g - 1)
+    :param bit: 原值
+    :param g: 哈希空间
+    :return: 扰动后的值
+    """
+    q = (1 - p) / (g - 1)
+    rnd = np.random.random_sample()
+    if rnd > p - q:
+        perturbed_bit = np.random.randint(0, g)
+    else:
+        perturbed_bit = bit
+    return perturbed_bit
+
+
+def support(v, report, g, n):
+    """
+    这个support函数是和我自己写的bf下的hash相匹配
+    :param v:
+    :param report:
+    :param g:
+    :param n:
+    :return:
+    """
+    c = 0
     for j in range(n):
-        if bf.hash(v,g,j)==report[j]:
-            c=c+1
+        if bf.hash(v, g, j) == report[j]:
+            c = c + 1
     return c
 
-def aggregation(c,n,g,p):
+def olh_support(v, report, g, n):
+    """
+    这个support函数是和我自己写的bf下的hash相匹配
+    :param v:
+    :param report:
+    :param g:
+    :param n:
+    :return:
+    """
+    c = 0
+    for j in range(n):
+        if olh_hash(v, g, j) == report[j]:
+            c = c + 1
+    return c
+
+def aggregation(c, n, g, p):
     """
     估计频率函数
     :param c: support 输入为x的报告的个数
@@ -36,5 +94,5 @@ def aggregation(c,n,g,p):
     :param p: 概率p
     :return:
     """
-    est=(c-n/g)/(p-1/g)
+    est = (c - n / g) / (p - 1 / g)
     return est
